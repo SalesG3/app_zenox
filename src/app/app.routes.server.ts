86a9d1d225ea8bdc8033363f1e744a6a759cc768
Sales <salesg3.fatorsistemas@gmail.com>
2026-07-05 00:27:52 -0300

    correção deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: ':alias/**',
+    // Mude para Server (SSR real-time) ou Client (Renderiza no navegador como o Angular antigo)
+    renderMode: RenderMode.Client 
+  },
+  {
+    // Opcional: Manter o resto do site (como a tela de login) como Prerender
     path: '**',
     renderMode: RenderMode.Prerender
   }
